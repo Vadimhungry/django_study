b867c50d5f83dad57ff46f2807c8a3beed58f50c
VadimK <yaralavadim@gmail.com>
2023-11-15 13:41:38 +0200

    templates add

--- django_study/article/views.py
+++ django_study/article/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+import sys
 
+# имя текущего приложения
+current_app_name = __package__ or (sys.argv[0] and sys.argv[0].split("/")[-2])
+
+test_name = 'QWERTY_name'
 
 def index(request):
-    return HttpResponse('article')
+    return render(
+        request, 'article/index.html',
+        context={'app_name': current_app_name,}
+    )
